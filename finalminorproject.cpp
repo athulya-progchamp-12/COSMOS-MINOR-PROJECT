c88bc5b2695973ef0d5a2baa7156723fa470c5b0
athulya-progchamp-12 <95928061+athulya-progchamp-12@users.noreply.github.com>
2022-02-10 21:39:47 +0530

    Add files via upload

---
+++ finalminorproject.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+    float x,y;
+    
+    char z;
+
+    cout<<"----------SIMPLE CALCULATOR----------"<<endl;
+
+    cout<<" Enter any two numbers = ";
+
+    cin>>x;
+    cin>>y;
+
+
+    cout<<" Enter an operator :"<<endl;
+    cout<<" + ( For Adding)"<<endl;
+    cout<<" - ( For Subtracting)"<<endl;
+    cout<<" * ( For Multiplying)"<<endl;
+    cout<<" / ( for Dividing) "<<endl;
+
+    cin>> z;
+
+   switch (z)
+   {
+   case '+':
+       cout<<"You chose Addition: "<<endl<< x << " + " << y <<" = " << x+y;
+      break;
+
+    case '-':
+       cout<<"You chose to Subtract: "<<endl<< x << " - " << y <<" = " << x-y;
+       break;
+
+    case '*':
+       cout<<"You chose to Multiply: "<<endl<< x << " * " << y <<" = " << x*y;
+       break;
+
+    case '/':
+       cout<<"You chose to Divide: "<<endl<< x << " / " << y <<" = " << x/y;
+       break;
+
+   default:
+      cout<< "Sorry!! Something Went Wrong";
+      break;
+
+   }
+
+
+    return 0;
+}
+
+
+
+
